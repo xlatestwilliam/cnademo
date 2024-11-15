e152641138bd4673fe9b81ed027f217f03cd903f
William Wong <w.wong@xsolla.com>
2024-11-15 11:39:51 +0800

    refactor: resolve navigator error

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
-'use client'
+// 'use client'
 
-import dynamic from 'next/dynamic'
+// import dynamic from 'next/dynamic'
 
-const DynamicWidget = dynamic(() => import('./components/loginWidget'), { ssr: false })
+// const DynamicWidget = dynamic(() => import('./components/loginWidget'), { ssr: false })
+
+// export default function Home() {
+
+//   return <DynamicWidget/>
+// }
+
+
+import Stage from './components/loginWidget'
 
 export default function Home() {
-
-  return <DynamicWidget/>
+  return (
+    <Stage/>
+  );
 }
